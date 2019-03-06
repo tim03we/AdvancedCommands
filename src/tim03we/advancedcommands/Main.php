ef4da117b3482e2b0fbee3fca0b58952897ec3d7
Tim <tim.wenderholm1931@gmail.com>
2019-03-06 15:18:50 +0100

    Added /repair

--- src/tim03we/advancedcommands/Main.php
+++ src/tim03we/advancedcommands/Main.php
@@ -9,19 +9,26 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
+use tim03we\advancedcommands\Events\EventListener;
+use tim03we\advancedcommands\commands\GamemodeCommand;
+use tim03we\advancedcommands\commands\FlyCommand;
+use tim03we\advancedcommands\commands\HealCommand;
+use tim03we\advancedcommands\commands\FeedCommand;
+use tim03we\advancedcommands\commands\VanishCommand;
+use tim03we\advancedcommands\commands\RepairCommand;
 
 class Main extends PluginBase implements Listener{
 
 	public function configUpdater(): void {
         $settings = new Config($this->getDataFolder() . "settings.yml", Config::YAML);
         $messages = new Config($this->getDataFolder() . "messages.yml", Config::YAML);
-		if($settings->get("version") !== "1.0.1"){
+		if($settings->get("version") !== "1.0.3"){
 			rename($this->getDataFolder() . "settings.yml", $this->getDataFolder() . "settings_old.yml");
 			$this->saveResource("settings.yml");
             $this->getLogger()->notice("We create a new settings.yml file for you.");
             $this->getLogger()->notice("Because the config version has changed. Your old configuration has been saved as settings_old.yml.");
 		}
-        if($messages->get("version") !== "1.0.4"){
+        if($messages->get("version") !== "1.0.6"){
 			rename($this->getDataFolder() . "messages.yml", $this->getDataFolder() . "messages_old.yml");
 			$this->saveResource("messages.yml");
             $this->getLogger()->notice("We create a new messages.yml file for you.");
@@ -34,6 +41,7 @@ class Main extends PluginBase implements Listener{
         $this->saveResource("messages.yml");
         $settings = new Config($this->getDataFolder() . "settings.yml", Config::YAML);
         $this->configUpdater();
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         if($settings->get("gamemode", true)) {
             Server::getInstance()->getCommandMap()->unregister(Server::getInstance()->getCommandMap()->getCommand("gamemode"));
             $this->getServer()->getCommandMap()->register("gamemode", new GamemodeCommand($this));
@@ -49,6 +57,9 @@ class Main extends PluginBase implements Listener{
         }
         if($settings->get("vanish", true)) {
             $this->getServer()->getCommandMap()->register("vanish", new VanishCommand($this));
+        }
+        if($settings->get("repair", true)) {
+            $this->getServer()->getCommandMap()->register("repair", new RepairCommand($this));
         }
 	}
 
